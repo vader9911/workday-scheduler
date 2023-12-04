50473a03d7bf1739d46ea78d4a48b89cb5cbb73d
Jeffrey Scott <82685075+vader9911@users.noreply.github.com>
2023-12-03 19:58:32 -0500

    duplicate rows, start working on time change

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,14 +1,84 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  
+  $('.btn').click(function (e) { 
+    e.preventDefault();
+    
+  }); 
+  var timeBlockAm =  $('.time-block-am').prop('outerHTML');
+  var timeBlockPm =  $('.time-block-pm').prop('outerHTML');
+  var containerAm = $('.container-lg-am');
+  var containerPm = $('.container-lg-pm');
+  var timeCardsAm = 12;
+  var timeCardsPm = 12;
+  var timeAm = 1; 
+  var timePm = 1;
+
+  
+for (let index = 1; index < timeCardsAm; index++) {
+  containerAm.append(timeBlockAm);
+
+  $('.time-block-am').addClass(function( index ) {
+    return "card-" + index; 
+  });
+
+  for (let index = 0; index < timeAm; index++) {
+    if (timeAm < 13){
+      console.log(timeAm);
+      timeAm == timeAm++;  
+  }
+  $('.card-'+timeAm).text(timeAm);
+  
+ }
+}
+
+
+
+
+// for (let index = 0; index < timeCardsPm; index++) {
+//   containerPm.append(timeBlockPm);
+//   $('.time-block-pm').addClass(function( index ) {
+//     return "card-" + index; 
+//   });
+  
+
+
+
+
+
+
+
+
+// for (let index = 0; index < hours; index++) {
+// $("div#timeAm").text(timeAm+'am');  
+// timeAm++;
+// }
+
+// if (timeAm < 12) {
+//   $("div#timeAm").text(timeAm+'am');  
+  
+// }else{
+//   timeAm++;
+// };
+
+
+
+// if (timeCards !== 24){
+//   $($("div.row")).appendTo($('.container-lg'));
+ 
+// }
+
+
+  // $('.time-block').appendTo("div.row.time-block");
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -18,6 +88,21 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+
+
+
+
+  
+// Display Day and time
+var currentTime = dayjs().format('h a');
+var currentDay =  dayjs().day(12).format(' dddd');
+
+
+$("p#currentDay").text('Today is: ' + currentDay);
+$("p#currentTime").text('Time: ' +currentTime);
+
+
 });
+
+
