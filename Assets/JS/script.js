ab63cd05924fd105597f282e67d206ec24559501
Jeffrey Scott <82685075+vader9911@users.noreply.github.com>
2023-12-04 23:11:30 -0500

    add click event and save text area to local

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,14 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+
   
   // Display Day and time
   var currentTime = dayjs().format('h a');
@@ -18,18 +10,40 @@ $(document).ready(function () {
   console.log(currentTime);
   $("p#currentDay").text('Today is: ' + currentDay);
   $("p#currentTime").text('Time: ' + currentTime);
-
-
-
-  $('.btn').click(function (e) { 
+  
+  $('.container-lg-am').on('click', '.time-block-am button', function (e) {
     e.preventDefault();
+
+    console.log('Save button clicked');
+    // Get the textarea that corresponds to the clicked button
+    var currentTextArea = $(this).closest('.time-block-am').find('textarea');
     
-  }); 
+    var textareaId = currentTextArea.attr('id');
+    // Get the text from the textarea
+    var inputValue = currentTextArea.val();
+    var key = 'textAm_' + textareaId;
+    // Store the text in local storage with the textareaId as the key
+    localStorage.setItem(key, inputValue);
+    console.log('Text stored in local storage with key:', key);
+});
+
+$('.container-lg-pm').on('click', '.time-block-pm button', function (e) {
+  e.preventDefault();
+
+  console.log('Save button clicked');
+  // Get the textarea that corresponds to the clicked button
+  var currentTextArea = $(this).closest('.time-block-pm').find('textarea');
+  var textareaId = currentTextArea.attr('id');
+  // Get the text from the textarea
+  var inputValue = currentTextArea.val();
+  var key = 'textPm_' + textareaId;
+  // Store the text in local storage with the textareaId as the key
+  localStorage.setItem(key, inputValue);
+});
 
 
-
-  var timeBlockAm =  $('.time-block-am').prop('outerHTML');
-  var timeBlockPm =  $('.time-block-pm').prop('outerHTML');
+  var timeBlockAm =  $('.time-block-am');
+  var timeBlockPm =  $('.time-block-pm');
   var containerAm = $('.container-lg-am');
   var containerPm = $('.container-lg-pm');
   var timeCardsAm = 12;
@@ -37,64 +51,42 @@ $(document).ready(function () {
 
 //loops to generate the time blocks
 for (let index = 1; index <= timeCardsAm; index++) {
-  containerAm.append(timeBlockAm);
-  var cardTime = '.card-' + index;
+  containerAm.append(timeBlockAm.clone());
   var assembledTimeAm = index + ' am';
   var currentCard = containerAm.find('.time-block-am').last();
-  currentCard.removeClass('hidden').addClass(cardTime);
+  var currentCardText = currentCard.find('.description');
+
+  currentCard.removeClass('hidden').addClass('card-' + index);
   currentCard.find('.hour').text(assembledTimeAm);
-  if (assembledTimeAm == currentTimeNum){
+  currentCardText.attr('id', index);
+
+  // Add check to color blocks
+  if (index == currentTimeNum && currentTimeAPm == 'am') {
     currentCard.addClass("present");
   }
-  if (currentTimeNum < index){
-    if (currentTimeAPm ==  'am'){
-      currentCard.addClass("future");
-    }
+  if (currentTimeNum < index && currentTimeAPm == 'am') {
+    currentCard.addClass("future");   
   }
 }
 
 for (let index = 1; index <= timeCardsPm; index++) {
-  containerPm.append(timeBlockPm);
-  var cardTime = '.card-pm-' + index;
+  containerPm.append(timeBlockPm.clone());
   var assembledTimePm = index + ' pm';
   var currentCard = containerPm.find('.time-block-pm').last();
-  currentCard.removeClass('hidden').addClass(cardTime);
+  var currentCardText = currentCard.find('.description');
+
+  currentCard.removeClass('hidden').addClass('card-' + index);
   currentCard.find('.hour').text(assembledTimePm);
-  if (assembledTimePm == currentTime){
+  currentCardText.attr('id', index);
+
+  // Add check to color blocks
+  if (index == currentTimeNum && currentTimeAPm == 'pm') {
     currentCard.addClass("present");
   }
-  if (currentTimeNum < index){
-    if (currentTimeAPm == 'pm'){
-      currentCard.addClass("future");
-    }
+  if (currentTimeNum < index && currentTimeAPm == 'pm') {
+    currentCard.addClass("future");   
   }
 }
-
-
-
-var nowCardAm = assembledTimeAm;
-// var nowCardPm = assembledTimePm;
-
-// if (nowCardAm == currentTime){
-  
-// }else if (nowCardPm == currentTime)
-
-
-// console.log(nowCardAm);
-  
-
-
-
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
 
 });
 
